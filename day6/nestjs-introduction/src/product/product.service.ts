7c73b2cf66cd61d9e463490dc6e69f20fd1ea85a
adityathakur-78 <adityathakur@medianv.com>
2025-12-31 23:42:49 +0530

    BD-5839: KRA - NestJS Introduction

---
+++ day6/nestjs-introduction/src/product/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+  private products = [
+    { id: 1, name: 'mobile', price: 20000 },
+    { id: 2, name: 'tablet', price: 40000 },
+    { id: 3, name: 'Laptop', price: 80000 },
+  ];
+  getAllProducts() {
+    return this.products;
+  }
+  getProductById(id: number) {
+    return this.products.find((products) => products.id == id);
+  }
+}
