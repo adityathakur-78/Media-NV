29aca95b5b3c0c0a80e1f63b49d2176ce7eba436
adityathakur-78 <adityathakur@medianv.com>
2026-01-05 23:31:03 +0530

    BD-5849: KRA - NestJS Middleware & Exception Filters

---
+++ day8/nest-middleware-exception/src/app.module.ts
@@ -0,0 +1,37 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { CitiesModule } from './cities/cities.module';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
+import { ExceptionController } from './exception/exception.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true,
+      }),
+    }),
+    CitiesModule,
+  ],
+  controllers: [AppController, ExceptionController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
